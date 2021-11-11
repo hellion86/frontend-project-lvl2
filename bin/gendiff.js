010a407606b14db30a34ef516f9f9995fd9031a7
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2021-11-11 07:16:57 +0700

    step 4, prepare tests

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -13,8 +13,8 @@ program
   .version('0.0.2')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    const result1 = readFileSync(path.resolve(filepath1), 'utf-8');
-    const result2 = readFileSync(path.resolve(filepath2), 'utf-8');
-    genDiff(result1, result2);
+    const firstConfig = readFileSync(path.resolve(filepath1), 'utf-8');
+    const secondConfig = readFileSync(path.resolve(filepath2), 'utf-8');
+    genDiff(firstConfig, secondConfig);
   })
   .parse();
