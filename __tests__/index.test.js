718a5cffd9fbd17e6128bab2128fc8a00e3250c9
Hexlet-Machine <Hexlet-Machine@yandex.ru>
2021-11-12 16:20:05 +0700

    change work with fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,13 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { test, expect } from '@jest/globals';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const firstConfig = getFixturePath('file1.json');
 const secondConfig = getFixturePath('file2.json');
@@ -16,13 +18,6 @@ const secondConfig = getFixturePath('file2.json');
 */
 
 test('genDiff firstConfig of secondConfig', () => {
-  const expected = `{
-- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
-}`;
-  expect(genDiff(firstConfig, secondConfig)).toEqual(expected);
+  const expected = readFile('diffResult.txt');
+  expect(genDiff(firstConfig, secondConfig)).toBe(expected);
 });
