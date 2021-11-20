ab7e4b6b471e15fd24a7d37141caeb144d32d6e5
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2021-11-20 22:47:11 +0700

    do step6

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 import _ from 'lodash';
 import { readFileSync } from 'fs';
 import { resolve, extname } from 'path';
-import { load } from 'js-yaml';
-
-export const parse = (data, extension) => ((extension === '.yml' || extension === '.yaml') ? load(data) : JSON.parse(data));
+import stylish from './stylish.js';
+import { isObject, normalized, parse } from './utils.js';
 
 export const genDiff = (filePath1, filePath2) => {
-  const obj1 = parse(readFileSync(resolve(filePath1), 'utf-8'), extname(filePath1));
-  const obj2 = parse(readFileSync(resolve(filePath2), 'utf-8'), extname(filePath2));
-  const allKeys = _.uniq([..._.keys(obj1), ..._.keys(obj2)]).sort();
-  const resultDiff = allKeys.reduce((acc, key) => {
-    if (!_.has(obj1, key)) {
-      acc[`+ ${key}`] = obj2[key];
-    } else if (!_.has(obj2, key)) {
-      acc[`- ${key}`] = obj1[key];
-    } else if (obj2[key] !== obj1[key]) {
-      acc[`- ${key}`] = obj1[key];
-      acc[`+ ${key}`] = obj2[key];
-    } else {
-      acc[`  ${key}`] = obj1[key];
-    }
-    return acc;
-  }, {});
-  const displayDiff = Object.entries(resultDiff).map(([key, value]) => (`${key}: ${value}`));
-  return `{\n${displayDiff.join('\n')}\n}`;
+  const firsObject = parse(readFileSync(resolve(filePath1), 'utf-8'), extname(filePath1));
+  const secondObject = parse(readFileSync(resolve(filePath2), 'utf-8'), extname(filePath2));
+  const makeTreeDifference = (obj1, obj2) => {
+    const allKeys = _.union(_.keys(obj1), _.keys(obj2)).sort();
+    return allKeys.map((key) => {
+      const value1 = normalized(obj1[key]);
+      const value2 = normalized(obj2[key]);
+      if (!_.has(obj1, key)) {
+        return { key, value: value2, status: 'added' };
+      }
+      if (!_.has(obj2, key)) {
+        return { key, value: value1, status: 'deleted' };
+      }
+      if (value2 !== value1) {
+        if (isObject(value1) && isObject(value2)) {
+          return { key, value: makeTreeDifference(value1, value2), status: 'nested' };
+        }
+        return {
+          key, value: value1, oldValue: value2, status: 'changed',
+        };
+      }
+      return { key, value: value1, status: 'unchanged' };
+    });
+  };
+  return `${stylish(makeTreeDifference(firsObject, secondObject))}`;
 };
+
+export default genDiff;
---
+++ src/stylish.js
@@ -0,0 +1,44 @@
+import { isObject, normalized } from './utils.js';
+
+const stylish = (diff, counter = 4) => {
+  const stringify = (row, sp, count) => {
+    if (!isObject(row)) {
+      return `${row}`;
+    }
+    const iter = (obj, cups, num) => {
+      const result = Object.entries(obj).map(([key, value]) => {
+        if (isObject(normalized(value))) {
+          return `${' '.repeat(num)}${key}: {\n${iter(value, ' ', num + 4)}`;
+        }
+        return `${' '.repeat(num)}${key}: ${value}`;
+      });
+      return `${result.join('\n')}\n${' '.repeat(num - 4)}}`;
+    };
+    const items = iter(row, sp, count);
+    return `{\n${items}`;
+  };
+
+  const assembledTree = (obj, num) => {
+    const tree = obj.map((record) => {
+      switch (record.status) {
+        case 'nested':
+          return `${' '.repeat(num)}${record.key}: {\n${assembledTree(record.value, num + counter)}\n${' '.repeat(num)}}`;
+        case 'changed':
+          return `${' '.repeat(num - 2)}- ${record.key}: ${stringify(record.value, ' ', num + counter)}\n${' '.repeat(num - 2)}+ ${record.key}: ${stringify(record.oldValue, ' ', num)}`;
+        case 'added':
+          return `${' '.repeat(num - 2)}+ ${record.key}: ${stringify(record.value, ' ', num + counter)}`;
+        case 'deleted':
+          return `${' '.repeat(num - 2)}- ${record.key}: ${stringify(record.value, ' ', num + counter)}`;
+        case 'unchanged':
+          return `${' '.repeat(num)}${record.key}: ${stringify(record.value, ' ', num)}`;
+        default:
+      }
+      return true;
+    }).join('\n');
+    return tree;
+  };
+  const displayDiff = assembledTree(diff, counter);
+  return `{\n${displayDiff}\n}`;
+};
+
+export default stylish;
