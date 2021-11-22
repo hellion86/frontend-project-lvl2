09e4f7ce27fc1a10aa842efda5ceaf985f2c1fb5
Hexlet-Machine <Hexlet-Machine@yandex.ru>
2021-11-22 10:24:17 +0700

    change test names

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -14,27 +14,27 @@ const secondJsonConfig = getFixturePath('file2.json');
 const firstYamlConfig = getFixturePath('file1.yml');
 const secondYamlConfig = getFixturePath('file2.yml');
 
-test('Test stylish YAML diff', () => {
+test('first stylish YAML diff', () => {
   const expected = readFile('diffResultStylish.txt');
   expect(genDiff(firstYamlConfig, secondYamlConfig, 'stylish')).toBe(expected);
 });
 
-test('Test stylish JSON diff', () => {
+test('secont stylish JSON diff', () => {
   const expected = readFile('diffResultStylish.txt');
   expect(genDiff(firstJsonConfig, secondJsonConfig, 'stylish')).toBe(expected);
 });
 
-test('Test plain YAML diff', () => {
+test('first plain YAML diff', () => {
   const expected = readFile('diffResultPlain.txt');
   expect(genDiff(firstYamlConfig, secondYamlConfig, 'plain')).toBe(expected);
 });
 
-test('Test plain JSON diff', () => {
+test('second plain JSON diff', () => {
   const expected = readFile('diffResultPlain.txt');
   expect(genDiff(firstJsonConfig, secondJsonConfig, 'plain')).toBe(expected);
 });
 
-test('Test json parser of JSON diff', () => {
+test('first json parser of JSON diff', () => {
   const expected = readFile('diffResultJSON.txt');
   expect(genDiff(firstJsonConfig, secondJsonConfig, 'json')).toBe(expected);
 });
