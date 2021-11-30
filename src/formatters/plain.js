cec4cb69e85770ea0ce4d2e3545f08c90c6f054c
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2021-11-30 09:41:01 +0700

    check #3

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,20 +12,18 @@ const plain = (diff) => {
   const assembledTree = (data, path = '') => {
     const tree = data.filter((item) => item.status !== 'unchanged')
       .map((record) => {
-      const fullPath = `${path}.${record.key}`;
-      switch (record.status) {
-        case 'nested':
-          return assembledTree(record.value, fullPath);
-        case 'added':
-          return `Property '${fullPath.slice(1)}' was added with value: ${normalizedValueName(record.value)}`;
-        case 'deleted':
-          return `Property '${fullPath.slice(1)}' was removed`;
-        case 'changed':
-          return `Property '${fullPath.slice(1)}' was updated. From ${normalizedValueName(record.oldValue)} to ${normalizedValueName(record.value)}`;
-        default:
-          return false;
-      }
-    }).join('\n');
+        const fullPath = `${path}.${record.key}`;
+        switch (record.status) {
+          case 'added':
+            return `Property '${fullPath.slice(1)}' was added with value: ${normalizedValueName(record.value)}`;
+          case 'deleted':
+            return `Property '${fullPath.slice(1)}' was removed`;
+          case 'changed':
+            return `Property '${fullPath.slice(1)}' was updated. From ${normalizedValueName(record.oldValue)} to ${normalizedValueName(record.value)}`;
+          default:
+            return assembledTree(record.value, fullPath);
+        }
+      }).join('\n');
     return tree;
   };
   return assembledTree(diff);
